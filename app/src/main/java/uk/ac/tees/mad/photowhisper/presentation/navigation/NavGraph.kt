f0c224097e20b44fc9e956233995d7a44d5c4c00
Mekala Sai <photowhisper4@gmail.com>
2026-03-10 12:00:00 +0530

    Theme, Color and Typography changed

--- app/src/main/java/uk/ac/tees/mad/photowhisper/presentation/navigation/NavGraph.kt
+++ app/src/main/java/uk/ac/tees/mad/photowhisper/presentation/navigation/NavGraph.kt
@@ -2,6 +2,9 @@ package uk.ac.tees.mad.photowhisper.presentation.navigation
 
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 @Composable
 fun NavGraph(
@@ -13,48 +16,48 @@ fun NavGraph(
         startDestination = startDestination
     ) {
         composable(Screen.Splash.route) {
-            SplashScreen(
-                onNavigateToHome = {
-                    navController.navigate(Screen.Home.route) {
-                        popUpTo(Screen.Splash.route) { inclusive = true }
-                    }
-                },
-                onNavigateToAuth = {
-                    navController.navigate(Screen.Login.route) {
-                        popUpTo(Screen.Splash.route) { inclusive = true }
-                    }
-                }
-            )
+//            SplashScreen(
+//                onNavigateToHome = {
+//                    navController.navigate(Screen.Home.route) {
+//                        popUpTo(Screen.Splash.route) { inclusive = true }
+//                    }
+//                },
+//                onNavigateToAuth = {
+//                    navController.navigate(Screen.Login.route) {
+//                        popUpTo(Screen.Splash.route) { inclusive = true }
+//                    }
+//                }
+//            )
         }
 
         composable(Screen.Login.route) {
-            LoginScreen(
-                onNavigateToRegister = {
-                    navController.navigate(Screen.Register.route)
-                },
-                onNavigateToHome = {
-                    navController.navigate(Screen.Home.route) {
-                        popUpTo(Screen.Login.route) { inclusive = true }
-                    }
-                }
-            )
+//            LoginScreen(
+//                onNavigateToRegister = {
+//                    navController.navigate(Screen.Register.route)
+//                },
+//                onNavigateToHome = {
+//                    navController.navigate(Screen.Home.route) {
+//                        popUpTo(Screen.Login.route) { inclusive = true }
+//                    }
+//                }
+//            )
         }
 
         composable(Screen.Register.route) {
-            RegisterScreen(
-                onNavigateToLogin = {
-                    navController.popBackStack()
-                },
-                onNavigateToHome = {
-                    navController.navigate(Screen.Home.route) {
-                        popUpTo(Screen.Register.route) { inclusive = true }
-                    }
-                }
-            )
+//            RegisterScreen(
+//                onNavigateToLogin = {
+//                    navController.popBackStack()
+//                },
+//                onNavigateToHome = {
+//                    navController.navigate(Screen.Home.route) {
+//                        popUpTo(Screen.Register.route) { inclusive = true }
+//                    }
+//                }
+//            )
         }
 
         composable(Screen.Home.route) {
-            HomeScreen()
+            //HomeScreen()
         }
     }
 }
--- app/src/main/java/uk/ac/tees/mad/photowhisper/ui/theme/Color.kt
+++ app/src/main/java/uk/ac/tees/mad/photowhisper/ui/theme/Color.kt
@@ -1,11 +1,15 @@
-package uk.ac.tees.mad.photowhisper.ui.theme
+package uk.ac.tees.mad.photowhisper.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Cream = Color(0xFFFFF8E7)
+val Beige = Color(0xFFF5E6D3)
+val MutedBrown = Color(0xFF8B7355)
+val DarkBrown = Color(0xFF5D4E37)
+val PastelPeach = Color(0xFFFFDAB9)
+val PastelBlush = Color(0xFFFFE4E1)
+val SoftGray = Color(0xFFE8E8E8)
+val TextPrimary = Color(0xFF3E2723)
+val TextSecondary = Color(0xFF6D4C41)
+val ErrorRed = Color(0xFFD32F2F)
+val SuccessGreen = Color(0xFF388E3C)
